51e2769436d4e394e191d9db0c980b7a57853b20
Raul Buffon <raulbuffon1999@gmail.com>
2021-10-25 20:35:34 -0300

    Update Migrations with new category relationship

---
+++ Migrations/20211025023859_Create Category and relation with TodoItem.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace just_todo_list_api.Migrations
+{
+    public partial class CreateCategoryandrelationwithTodoItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "TodoItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItems_CategoryId",
+                table: "TodoItems",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoItems_Categories_CategoryId",
+                table: "TodoItems",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoItems_Categories_CategoryId",
+                table: "TodoItems");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TodoItems_CategoryId",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "TodoItems");
+        }
+    }
+}
